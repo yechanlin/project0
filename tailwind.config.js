0733ac59e41098f926de8628d41365571e5c047c
Sai Aung Min Khant <saiaungminkhant20@gmail.com>
2024-11-24 11:38:24 +0700

    install tailwind and react-router-dom

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5c6c90',
+        secondary: '#8d95b4',
+      },
+    },
+  },
+  plugins: [],
+};
